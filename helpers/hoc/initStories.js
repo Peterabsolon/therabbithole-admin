44c47a8a561c3d65c3bc179140218f3187e5a01c
vvanek <xvanv12@gmail.com>
2017-07-18 11:49:33 +0200

    :memo: storybook background

--- helpers/hoc/initStories.js
+++ helpers/hoc/initStories.js
@@ -3,7 +3,9 @@ import { Provider as ThemeProvider } from 'rebass'
 import XRay from 'react-x-ray'
 import styled from 'styled-components'
 
-const Wrapper = styled.footer`min-height: ${props => props.minHeight ? 'auto' : '100vh'};`
+const Wrapper = styled.footer`
+  min-height: ${props => props.minHeight ? 'auto' : '100vh'};
+`
 // Theme
 import defaultTheme from 'styles/theme/defaultTheme'
 
@@ -22,7 +24,7 @@ export default class Welcome extends React.Component {
         <button
           onClick={() => this.toggle()}
           children="x-ray"
-          style={{ position: 'absolute', right: 10, top: 10 }}
+          style={{ position: 'absolute', right: 10, bottom: 10 }}
         />
         <XRay disabled={this.state.xray} color="#f0f" backgroundColor="#200020">
           <Wrapper minHeight={this.state.xray}>{this.props.children}</Wrapper>{' '}
